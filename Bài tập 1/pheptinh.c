b1c77950d25f33b00a0695ddae2a87e6eb4ee66b
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2020-11-26 10:40:19 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-int main() {
-	int a , b;
-	a = 3 , b =5
-scanf("%d%d", &a, &b);
-	int sum = a + b;
-	int mul = a * b;
-	int sub = a - b;
-	int div = a / b;
+int main(){
+	//khai bao bien a, b kieu int
+	int a, b;
+	//khoi tao bien a = 3
+	a = 3;
+	//khoi tao bien b = 5
+	b = 5;
+	//khai bao bien tong, hieu, tich, thuong
+	int tong, hieu, tich;
+	double thuong;
+	tong = a + b;
+	hieu = a - b;
+	tich = a * b;
+	thuong = (double)(a / b );
+	printf("a + b = %d, a - b = %d, a * b = %d, a / b = %f",tong, hieu, tich, thuong);
+	return 0;
+}
 	
-	printf("%d + %d = %d\n", a , b, sum);
-	printf("%d * %d = %d\n", a , b, mul);
-	printf("%d - %d = %d\n", a , b, sub);
-	printf("%d / %d = %d\n", a , b, div);
-	return }
-    
 
