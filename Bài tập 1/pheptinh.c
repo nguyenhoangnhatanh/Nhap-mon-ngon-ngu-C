cd158e5b7effbadbe583f86053ae02921538d94a
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2020-11-26 10:48:14 +0700

    Update pheptinh.c

--- Bài tập 1/pheptinh.c
+++ Bài tập 1/pheptinh.c
@@ -10,12 +10,14 @@ int main(){
 	//khai bao bien tong, hieu, tich, thuong
 	int tong, hieu, tich;
 	double thuong;
+	//gan tong a+b, hieu = a-b, tich =a*b, thuong = a/b
 	tong = a + b;
 	hieu = a - b;
 	tich = a * b;
 	thuong = (double)(a / b );
+	//hien thi cac bien tong, hien, tich, thuong theo dinh dang: a + b = 8, a - b = -2, a * b = 15, a / b = 0
 	printf("a + b = %d, a - b = %d, a * b = %d, a / b = %f",tong, hieu, tich, thuong);
+	//thay doi khai bao bien thuong va ap dung ep kieu de ket qua a/b = 0.6
 	return 0;
 }
 	
-
