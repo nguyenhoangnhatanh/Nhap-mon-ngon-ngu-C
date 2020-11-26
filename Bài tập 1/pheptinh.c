a2c7022f57db3c2a0ef58b8bd0b8f3c96b63d462
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2020-11-26 09:05:34 +0700

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+	int a , b;
+	a = 3 , b =5
+	printf("nhap hai so nguyen a, b: ")
+    scanf("%d%d" , &a, &b");
+	int sum = a + b;
+	int mul = a * b;
+	int sub = a - b;
+	int div = a / b;
+	
+	printf("%d + %d = %d\n", a , b, sum);
+	printf("%d * %d = %d\n", a , b, mul);
+	printf("%d - %d = %d\n", a , b, sub);
+	printf("%d / %d = %d\n", a , b, div);
+	return }
+    
+
