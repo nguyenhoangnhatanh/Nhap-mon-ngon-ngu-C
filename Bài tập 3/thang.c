af2d9660601f2b3796a5b7b3b9853dddcf4dfa64
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2020-12-17 10:59:30 +0700

    Add files via upload

---
+++ Bài tập 3/thang.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+  int thang;
+  printf ("nhap thang:");
+  sacnf ("%d", &thang);
+  switch (thang){
+  	case 2:
+  		printf ("28 ngay"); break;
+  	case 3:
+	  	printf ("31 ngay"); break;
+	case 4:
+	   	printf ("30 ngay"); break;
+    case 5:
+    	printf ("31 ngay"); break;
+    case 6:
+	    printf ("30 ngay"); break;
+	case 7:
+		printf ("28 ngay"); break;
+	case 8:
+		printf ("30 ngay"); break;
+  		
+  	
+  	
+  	
+  	return 0;
+  }
