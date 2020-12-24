471a00952a17caa6f2832cda6501950fea4904e9
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2020-12-24 10:06:55 +0700

    Add files via upload

---
+++ Bài tập 3/xinchao.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+  int count ;
+      printf ("\t xin chao \n ");
+  for ( count =1 ; count <= 10 ; count ++ )
+   //hien thi ra man hinh 10 lan xin chao
+      printf (" \n \t \t xin chao \n");
+      
+  return 0;
+}
