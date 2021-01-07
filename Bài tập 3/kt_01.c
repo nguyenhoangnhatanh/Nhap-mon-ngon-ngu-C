2ab2e21de80058adc68fb21864c62cc5c09ac3c3
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2021-01-06 17:06:33 -0800

    Create kt_01.c

---
+++ Bài tập 3/kt_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+  int i, j;
+  printf ("\t                             BANG CUU CHUONG\n");
+//vong lap tu 1 den 10
+  for (i = 1; i <= 10; i++){
+//vong lap tu 1 den 10
+  for (j = 1; j <= 10; j++){
+//hien thi ban cuu chuong ra man hinh 
+  printf ("%dx%d=%d\t ", i, j, i*j);
+  }    
+  }
+return 0;
+
+}
