425b779dc1b1464448ff0b334aa99337e356351f
Nguyễn Hoàng Nhật Anh <72533533+nguyenhoangnhatanh@users.noreply.github.com>
2021-01-09 10:22:07 +0700

    Create kt_06.c

---
+++ Bài tập 3/kt_06.c
@@ -0,0 +1,29 @@
+#iclude <stdio.h>
+int main (){
+//h 4:4 hang
+//hang 1: 1 ngoi sao 6 khoanng trang
+//hang 2: 3 ngoi sao 4 khoanng trang
+//hang 3: 5 ngoi sao 2 khoanng trang
+//hang 4: 7 ngoi sao 0 khoanng trang
+
+//h 3:3 hang
+//hang 1: 1 ngoi sao 4 khoanng trang
+//hang 2: 3 ngoi sao 2 khoanng trang
+//hang 3: 4 ngoi sao 0 khoanng trang
+//hang 4: ? ngoi sao ? khoanng trang
+
+//h 2:2 hang 
+//hang 1: 1 ngoi sao 2 khoanng trang
+//hang 2: 3 ngoi sao 0 khoanng trang
+//hang 3: ? ngoi sao ? khoanng trang
+//hang 4: ? ngoi sao ? khoanng trang
+
+//h 5:5 hang
+//hang 1: 1 ngoi sao 8 khoanng trang
+//hang 2: 3 ngoi sao 6 khoanng trang
+//hang 3: 5 ngoi sao 4 khoanng trang
+//hang 4: 7 ngoi sao 2 khoanng trang
+
+
+return 0;
+}
